cc2afd2721542699e519ca87907d570377acc174
Bano Banov <banovb@hotmail.com>
2021-01-10 16:34:22 +0200

    Create 5-max-number-second-v2.js

---
+++ 3-arrays/5-max-number-second-v2.js
@@ -0,0 +1,24 @@
+//5. Max Number second (v2)
+
+function maxNum(arr) {
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let isBigger = true;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (Number(arr[i]) <= Number(arr[j])) {
+                isBigger = false;
+                break;
+            }
+        }
+        if (isBigger) {
+            newArr.push(arr[i]);
+        }
+    }
+    console.log(newArr.join(" "));
+}
+
+maxNum([1, 4, 3, 2]);
+maxNum([14, 24, 3, 19, 15, 17]);
+maxNum([41, 41, 34, 20]);
+maxNum([27, 19, 42, 2, 13, 45, 48]);
