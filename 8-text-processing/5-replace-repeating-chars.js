3ed04f8340a31c70033d1ae63ff82f949b04d91b
Bano Banov <banovb@hotmail.com>
2021-01-10 18:28:29 +0200

    Create 5-replace-repeating-chars.js

---
+++ 8-text-processing/5-replace-repeating-chars.js
@@ -0,0 +1,12 @@
+//5. Replace Repeating Chars 
+function replaceRepeatingChars(str) {
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+        str[i] !== str[i + 1] ? (result += str[i]) : "";
+    }
+
+    console.log(result);
+}
+
+replaceRepeatingChars("aaaaabbbbbcdddeeeedssaa");
+replaceRepeatingChars("qqqwerqwecccwd");
